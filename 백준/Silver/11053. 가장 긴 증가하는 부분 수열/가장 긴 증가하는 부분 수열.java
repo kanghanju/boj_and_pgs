3a52c030697a45446af4db8120a0a9a73934868e
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-01-27 21:27:12 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -10,27 +10,26 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n+1];
+        int[] a = new int[n];
 
-        int[] dp = new int[n+1];//dp[i]는 a[i]를 마지막으로 골랐을 때 최장 증가 부분 수열의 길이다.
+        int[] dp = new int[n];//dp[i]는 a[i]를 마지막으로 골랐을 때 최장 증가 부분 수열의 길이다.
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
             a[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1; //모든수의 가장 긴 증가 부분수열의 길이 최솟값이 1이다.
+            dp[i] = 1; //모든수의 가장 긴 증가 부분수
         }
 
 
         int max = 1;//가장 긴 증가 부분수열의 길이 max
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j < i; j++){
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
                 if(a[i] > a[j]){
                     dp[i] = Math.max(dp[i],dp[j]+1);
                 }
             }
             max = Math.max(max,dp[i]);
         }
-
         System.out.println(max);
     }
 }
