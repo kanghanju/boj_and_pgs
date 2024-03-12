66a8feaf255edeabeade8ec100a968785a3be518
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-03-12 17:33:29 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.93 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String s) {
+        String[] numbers = s.split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(String number : numbers) {
+            int num = Integer.parseInt(number);
+            
+            min = Math.min(min,num);
+            max = Math.max(max,num);
+        }
+        
+        return min + " " + max;
+    }
+}
