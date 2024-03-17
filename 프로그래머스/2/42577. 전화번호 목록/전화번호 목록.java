4df2c7b4771dcf0cabb63903208d186477f7016e
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-03-17 20:19:33 +0900

    [level 2] Title: 전화번호 목록, Time: 232.89 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+         Set<String> set = new HashSet<>();
+
+        for(String number : phone_book) {
+            set.add(number);
+        }
+
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 0; j < phone_book[i].length(); j++) {
+                if(set.contains(phone_book[i].substring(0,j))) return false;
+            }
+        }
+        
+        return true;
+    }
+}
