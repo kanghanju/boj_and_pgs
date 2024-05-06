e307c3ab1742e2c39c469c00f55740d272969de1
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-05-06 20:24:02 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.09 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,54 +1,21 @@
 class Solution {
-    static String[][] arr;
+    
 
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        arr = new String[n][n];
+         String[] answer = new String[n];
+        String temp;
 
         for (int i = 0; i < n; i++) {
-            String result1 = convertToBinary(arr1[i],n);
-            String result2 = convertToBinary(arr2[i],n);
+            temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+            temp = temp.substring(temp.length() - n);
+            temp = temp.replaceAll("1", "#");
+            temp = temp.replaceAll("0", " ");
 
-            check(result1, i);
-            check(result2, i);
-        }
-
-        for (int i = 0; i < n; i++) {
-            String s = "";
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] == null) {
-                    s += " ";
-                } else {
-                    s += "#";
-                }
-            }
-
-            answer[i] = s;
+            answer[i] = temp;
         }
 
         return answer;
     }
 
-    private String convertToBinary(int n,int num) {
-        StringBuilder sb = new StringBuilder();
-
-        while (n > 0) {
-            sb.append(n % 2);
-            n = n / 2;
-        }
-
-        while (sb.length() < num) {
-            sb.append("0");
-        }
-
-        return sb.reverse().toString();
-    }
-
-    private void check(String result, int row) {
-        for (int i = 0; i < result.length(); i++) {
-            if (result.charAt(i) == '1') {
-                arr[row][i] = "#";
-            }  
-        }
-    }
+    
 }
