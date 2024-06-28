23c0b69716de9b6c706a6f6d98e3986a8503334c
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-06-28 23:53:10 +0900

    [level 2] Title: 삼각 달팽이, Time: 14.20 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,42 +1,62 @@
 class Solution {
-    private static int[] dx = {1,0,-1};
-    private static int[] dy = {0,1,-1};
-    
     public int[] solution(int n) {
         int[][] triangle = new int[n][n];
-        int num = 1;
+        int v = 1; //채워넣을 숫자
         int x = 0;
-        int y = 0;
-        int dir = 0;
-        
-        while(true) {
-            triangle[x][y] = num++;
-            
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            
-            if(nx == n || ny == n || triangle[nx][ny] != 0) {
-                //방향 바꾸기 
-                dir = (dir + 1) % 3;
-                nx = x + dx[dir];
-                ny = y + dy[dir];
-                
-                if(nx == n || ny == n || triangle[nx][ny] != 0) break;        
+        int y = 0; //숫자를 넣을 위치
+
+        while (true) {//이동할 수 없을 때까지 채우기
+            //아래로 이동
+            while (true) {
+                triangle[x][y] = v++;
+                if (x == n - 1 || triangle[x + 1][y] != 0) {
+                    break;
+                }
+                x++;
             }
-        
-            
-            x = nx;
-            y = ny;
+            if ( y + 1 == n || triangle[x][y + 1] != 0 ) {
+                break;
+            }
+            y += 1;
+
+            //오른쪽으로 이동
+            while (true) {
+                triangle[x][y] = v++;
+                if (y == n - 1 || triangle[x][y + 1] != 0) {
+                    break;
+                }
+                y++;
+            }
+            if (triangle[x - 1][y - 1] != 0) {
+                break;
+            }
+            x -= 1;
+            y -= 1;
+
+            //왼쪽 위로 이동
+            while (true) {
+                triangle[x][y] = v++;
+                if (triangle[x - 1][y - 1] != 0) {
+                    break;
+                }
+                x -= 1;
+                y -= 1;
+            }
+            if ( x + 1 == n || triangle[x + 1][y] != 0) {
+                break;
+            }
+            x++; //방향 바꾸기
         }
-        
-        int[] result = new int[num - 1];
+
+        int[] result = new int[v - 1];
         int index = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j <= i; j++) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 result[index++] = triangle[i][j];
             }
         }
-        
+
         return result;
     }
 }
