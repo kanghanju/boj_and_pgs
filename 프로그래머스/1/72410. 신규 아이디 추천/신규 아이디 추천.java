08c872823a6ac3350a0c777424be123828644bd3
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-11-29 17:27:31 +0900

    [level 1] Title: 신규 아이디 추천, Time: 16.84 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,18 +1,37 @@
 class Solution {
     public String solution(String newId) {
+        //id는 3자 이상 15자 이하
+        //알파벳 소문자,숫자,-,_,.문자만 사용 가능
+        //.는 처음과 끝에 사용할 수 없으며 또한 연속으로 사용할 수 없다
+
         newId = newId.toLowerCase();
-        newId = newId.replaceAll("[^a-z0-9\\-_.]","");
-        newId = newId.replaceAll("\\.{2,}",".");
-        newId = newId.replaceAll("^\\.|\\.$","");
-        if(newId.isEmpty())newId += "a";
-        if(newId.length() >= 16) {
-            newId = newId.substring(0,15);
-            newId = newId.replaceAll("\\.+$","");
-        }
-        while (newId.length() <= 2) {
-            newId += newId.charAt(newId.length() - 1);
+        newId = newId.replaceAll("[^a-z0-9-_.]", "");
+        newId = newId.replaceAll("[.]{2,}", ".");
+
+        if (newId.startsWith(".")) {
+            newId = newId.substring(1);
         }
         
+        if (newId.endsWith(".")) {
+            newId = newId.substring(0, newId.length() - 1);
+        }
+
+        if (newId.isEmpty()) {
+            newId = "a";
+        }
+
+        if (newId.length() >= 16) {
+            newId = newId.substring(0, 15);
+
+            if (newId.endsWith(".")) {
+                newId = newId.substring(0, newId.length() - 1);
+            }
+        }
+
+        while (newId.length() < 3) {
+            newId += newId.charAt(newId.length() - 1);
+        }
+
         return newId;
     }
 }
