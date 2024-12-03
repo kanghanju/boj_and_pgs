8eaad290afffab4fea04ee6f80dd48c0ab25eb6c
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-12-03 20:29:31 +0900

    [Silver I] Title: 경로 찾기, Time: 304 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,53 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] graph,answer;
+
+    static int[][] graph;
+    static int[][] answer;
     static boolean[] visited;
-    static int nowNode;
-    static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
-        graph = new int[n][n]; //주어진 인접 행렬
-        answer = new int[n][n]; //정답 인접 행렬
+        int n = Integer.parseInt(br.readLine());
+        graph = new int[n][n];
+        answer = new int[n][n];
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++){
+            for (int j = 0; j < n; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        visited = new boolean[n];
-        for(int i = 0; i < n; i++){
-            Arrays.fill(visited,false);
-            nowNode = i;
-            dfs(i);
+        for (int i = 0; i < n; i++) {
+            visited = new boolean[n];
+            dfs(i, i);
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                System.out.print(answer[i][j]+" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(answer[i][j] + " ");
             }
             System.out.println();
         }
-
-
     }
 
-    static void dfs(int startRow){
-        for(int i = 0; i < n; i++){
-            if(graph[startRow][i] == 1 && visited[i] == false){
-                answer[nowNode][i] = 1;
+    static void dfs(int start, int currNode) {
+        //따로 종료조건을 넣어주지 않아도 알아서 루프를 돌고 나감
+
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[currNode][i] == 1 && !visited[i]) {//방문하지 않은 위치며, 연결되어있다면
                 visited[i] = true;
-                dfs(i);
+                answer[start][i] = 1;
+                dfs(start, i);
             }
         }
     }
-
 }
