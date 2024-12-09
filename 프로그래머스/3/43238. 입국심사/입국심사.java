37cbc6e2115b2c8ac186ace759e7a2c14fcff907
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-12-09 18:45:54 +0900

    [level 3] Title: 입국심사, Time: 101.82 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,26 +3,31 @@ import java.util.Arrays;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
-        
+
         Arrays.sort(times);
-        long left = times[0];
-        long right = (long) times[times.length - 1] * n;
+        long start = times[0];//가장 짧게 걸릴때
+        long end = (long) times[times.length - 1] * n; //가장 오래 걸릴때
 
-        while (left <= right) {
-            long mid = (left + right) / 2; //(최소시간 + 가장오래걸리는 시간) / 2
-            long complete = 0;
+        while (start < end) {
+            long mid = (start + end) / 2; //심사 받는데 걸리는 시간 최소의 후보
+            long complete = 0; //심사를 완료한 사람
 
-            for (int judgeTime : times) {//심사대별 심사 시간
-                complete += mid / judgeTime; //각 심사대별로 주어진 시간동안 몇 명을 심사할 수 있는지 -> 주어진 인원수를 최대한 심사대별로 나누는것(최소시간구할수있다)
+            for (int judgeTime : times) {
+                long judgeMaxNOP = mid / judgeTime; //각 심사위원이 주어진 최소시간동안 심사할 수 있는 인원 수
+                complete += judgeMaxNOP;
+
+                if (complete >= n) {
+                    break;
+                }
             }
-            
-            if (complete >= n) {//n명보다 오버해서 심사함. 즉 시간이 남는다.
-                right = mid - 1;
+
+            if (complete >= n) {//심사 완료 가능한 사람이 실제 주어진 사람수보다 많다면
+                end = mid;
                 answer = mid;
-            } else {//다 심사를 못함
-                left = mid + 1;
-                //answer = mid;가 여기에 오면 안된다! 그러면 심사를 다 못한다
+            } else {//심사 완료 시간이 부족하다면
+                start = mid + 1;
             }
+
         }
 
         return answer;
