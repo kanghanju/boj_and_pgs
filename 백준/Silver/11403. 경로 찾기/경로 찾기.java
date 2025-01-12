6b5c02b54f89f4ec66429e3444afa8a9d4f7ff06
kanghanju <66769475+kanghanju@users.noreply.github.com>
2025-01-12 17:14:32 +0900

    [Silver I] Title: 경로 찾기, Time: 188 ms, Memory: 19348 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,22 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int[][] graph;
-    static int[][] answer;
-    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
         graph = new int[n][n];
-        answer = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -25,28 +21,23 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            visited = new boolean[n];
-            dfs(i, i);
+        for (int k = 0; k < n; k++) {//경유지
+            for (int i = 0; i < n; i++) {//출발지
+                for (int j = 0; j < n; j++) {//도착지
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {//경로가 있다면
+                        graph[i][j] = 1;
+                    }
+                }
+            }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(answer[i][j] + " ");
+                sb.append(graph[i][j] + " ");
             }
-            System.out.println();
+            sb.append("\n");
         }
-    }
-
-    static void dfs(int start, int currNode) {
-        //따로 종료조건을 넣어주지 않아도 알아서 루프를 돌고 나감
-
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[currNode][i] == 1 && !visited[i]) {//방문하지 않은 위치며, 연결되어있다면
-                visited[i] = true;
-                answer[start][i] = 1;
-                dfs(start, i);
-            }
-        }
+        System.out.println(sb);
     }
 }
